c1d8ce28cf3d497a2ad24183727684f04e07b483
M Lintang Maulana Zulfan <lintangmaulanazulfan@gmail.com>
2026-03-06 23:38:10 +0700

    security: purge leaked build artifacts

--- app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_lifemarker_LifeMarkerApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.lifemarker.LifeMarkerApp"
-)
-public final class _com_lifemarker_LifeMarkerApp {
-}
